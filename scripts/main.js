fc6b92ea5d00b2712bf4e115523623af8ff8e6f6
Firepup650 <70233190+Firepup6500@users.noreply.github.com>
2024-11-05 16:55:03 -0600

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,9 +5,9 @@
 
 //Vars.enableConsole = true;
 
-//if(Vars.headless){
+if(Vars.headless){
 //    throw "no server support yet";
-//}else{
+}else{
     // This may break something, but I'm doing it anyways! (disabling the desktop only check)
     rpc();
     title();
@@ -17,7 +17,7 @@
         dialog = menu.setupDialog();
         menu.addSettings(dialog);
     });
-//}
+}
 
 // most pointless thing i have ever done
 function rpc(){
